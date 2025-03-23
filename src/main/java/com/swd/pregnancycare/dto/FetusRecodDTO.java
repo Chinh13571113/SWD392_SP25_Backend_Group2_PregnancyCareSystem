af5dbd427a890c5afbbc045278d85b907b30e2cc
Lê Quang Ánh <superanhziggy@gmail.com>
2025-03-23 11:26:58 +0700

    23/03/25_recordChart

--- src/main/java/com/swd/pregnancycare/dto/FetusRecodDTO.java
+++ src/main/java/com/swd/pregnancycare/dto/FetusRecodDTO.java
@@ -2,7 +2,9 @@ package com.swd.pregnancycare.dto;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import lombok.experimental.FieldDefaults;
 
 import java.math.BigDecimal;
@@ -13,6 +15,8 @@ import java.time.LocalDateTime;
 @JsonInclude(JsonInclude.Include.NON_NULL)
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class FetusRecodDTO {
     LocalDateTime dateRecord;
     BigDecimal weight;
---
+++ src/main/java/com/swd/pregnancycare/repository/WhoStandardRepo.java
@@ -0,0 +1,7 @@
+package com.swd.pregnancycare.repository;
+
+import com.swd.pregnancycare.entity.WhoStandardEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface WhoStandardRepo extends JpaRepository<WhoStandardEntity,Integer> {
+}
